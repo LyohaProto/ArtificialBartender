7c381a2e09b77804deccec2e9ad90dc7d087978c
Jack Ward <jack.ward@grabone.co.nz>
2018-10-26 16:01:40 +1300

    progress meter

--- setup.py
+++ setup.py
@@ -1,16 +1,29 @@
 import urllib.request
+import math
+import sys
 
 BASE_URL = 'https://s3-ap-southeast-2.amazonaws.com/artificial-bartender'
 bin_files = ['age_net.caffemodel', 'gender_net.caffemodel']
 
 for bin_file in bin_files:
 	s3_file = f'{BASE_URL}/{bin_file}'
-	with urllib.request.urlopen(s3_file) as response:
-		print(f"Downloading {s3_file}...")
-		content = response.read()
-		local_filename = f'age_gender_models/{bin_file}'
-		with open(local_filename, 'wb') as local_file:
-			print(f"Writing local file {local_filename}")
+	local_filename = f'age_gender_models/{bin_file}'
+	sys.stdout.write(f"Downloading {s3_file}... ")
+	with urllib.request.urlopen(s3_file) as response, open(local_filename, 'wb') as local_file:
+		total_bytes = response.length
+		chunk = 100 * 1024  # download 100kb chunks
+		content = response.read(chunk)
+		counter = 1
+		total_chunks = math.ceil(total_bytes / chunk)
+		while content:
 			local_file.write(content)
+			percentage = round(counter / total_chunks * 100, 1)
+			percentage_len = len(str(percentage)) + 10
+			content = response.read(100 * 1024)
+			counter += 1
+			sys.stdout.write(f"{percentage}% complete")
+			sys.stdout.flush()
+			sys.stdout.write("\b" * percentage_len)
+		sys.stdout.write("Completed.     \n")
 
 print("Done!")
